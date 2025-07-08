a1910f524c383e32674b66201aed5531fdd2ffe1
Gabriel dos Santos <49681659+gabrielsaDRC@users.noreply.github.com>
2025-07-08 19:24:54 -0300

    Corrigir configuração do GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/reset-corporativo/' : '/',
+  base: '/reset-corporativo/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
